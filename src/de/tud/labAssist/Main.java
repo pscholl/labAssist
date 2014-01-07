014b419cb3596e7816be361f89ddd3133e689b75
Philipp M. Scholl <scholl@ess.tu-darmstadt.de>
2014-01-07 14:47:47 +0100

    now working

--- src/de/tud/labAssist/Main.java
+++ src/de/tud/labAssist/Main.java
@@ -1,14 +1,9 @@
 package de.tud.labAssist;
 
-import com.google.glass.input.VoiceInputHelper;
-import com.google.glass.input.VoiceListener;
-import com.google.glass.logging.FormattingLogger;
-import com.google.glass.logging.FormattingLoggers;
-import com.google.glass.util.PowerHelper;
-import com.google.glass.voice.VoiceCommand;
-import com.google.glass.voice.VoiceConfig;
-import com.google.glass.voice.VoiceConfig.LetterToSoundModel;
-
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -17,6 +12,15 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
+
+import com.google.glass.input.VoiceInputHelper;
+import com.google.glass.util.PowerHelper;
+import com.google.glass.voice.VoiceCommand;
+import com.google.glass.voice.VoiceConfig;
 
 public class Main extends FragmentActivity {
 
@@ -24,7 +28,8 @@ public class Main extends FragmentActivity {
   protected static final float SWIPE_TRESHOLD = 400;
   private ViewPager mPager;
   private PagerAdapter mPagerAdapter;
-  private VoiceInputHelper voiceInputHelper;
+  private VoiceInputHelper mVoiceInputHelper;
+  private ScreenSlideScroller mScroller;
    
 
   @Override
@@ -36,16 +41,110 @@ public class Main extends FragmentActivity {
     mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
     mPager.setAdapter(mPagerAdapter);
     
-    voiceInputHelper = new VoiceInputHelper(this, new PageFlipper(),
+    mVoiceInputHelper = new VoiceInputHelper(this, new PageFlipper(),
         VoiceInputHelper.newUserActivityObserver(this));
-    voiceInputHelper.addVoiceServiceListener();
+    
+    mScroller = new ScreenSlideScroller(mPager);
+  }
+  
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mVoiceInputHelper.addVoiceServiceListener();
+    mScroller.start();
+  }
+  
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mVoiceInputHelper.removeVoiceServiceListener();
+    mScroller.stop();
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
     return true;
+  }
+  
+  private class ScreenSlideScroller implements SensorEventListener {
+
+    private static final int SENSOR_RATE_uS = 20000;
+    private static final float TO_ANGLE_RAD = .1F; // must be positive
+    private static final float FROM_ANGLE_RAD = -.1F;
+    private final float TOTAL_ANGLE = Math.abs(FROM_ANGLE_RAD) + TO_ANGLE_RAD;
+    private Sensor mSensor;
+    private int mLastAccuracy;
+    private SensorManager ss;
+    private PowerHelper mPower;
+    private int mLastNum;
+    private ScrollView mScrollView;
+    private View mChildView;
+    private ViewPager mPager;
+    
+    public ScreenSlideScroller(ViewPager pager) {
+      ss = (SensorManager) getSystemService(SENSOR_SERVICE);
+      mSensor = ss.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+      mPower = new PowerHelper(getApplicationContext());
+      mPager = pager;
+    }
+    
+    public void start() {
+      if (mSensor == null)
+        return;
+      
+      ss.registerListener(this, mSensor, SENSOR_RATE_uS);
+    }
+    
+    public void stop() {
+      ss.unregisterListener(this);
+    }
+    
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+      mLastAccuracy = accuracy;
+    }
+    
+    private void switchViews(int num) {
+      if (num == mLastNum  && mScrollView != null)
+        return; // nothing to do
+      
+      String tag  = String.format("card_view_%d",num);
+      mScrollView = (ScrollView) mPager.findViewWithTag(tag);
+      mChildView  = mScrollView.getChildAt(0);
+      mLastNum    = num;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+      if (mLastAccuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
+        return;
+      
+      float[] mat = new float[9], orientation = new float[3];
+      
+      // the remapping here is to fix the rotation order to Y-X-Z (in glass coordinates)
+      SensorManager.getRotationMatrixFromVector(mat, event.values);
+      SensorManager.remapCoordinateSystem(mat, SensorManager.AXIS_X, SensorManager.AXIS_Z, mat);
+      SensorManager.getOrientation(mat, orientation);
+      
+      float cur = orientation[1];
+      
+      if (cur < FROM_ANGLE_RAD || cur > TO_ANGLE_RAD) 
+        return; // nothing to do
+      
+      switchViews(mPager.getCurrentItem());
+      
+      int displayed_size = mScrollView.getHeight(),
+            content_size = mChildView.getHeight();
+      
+      if (content_size < displayed_size)
+        return; // nothing to do
+      
+      mPower.stayAwake(3*1000); // keep awake for 3 additional secs
+      
+      float factor = (((float) (content_size - displayed_size))/TOTAL_ANGLE);
+      mScrollView.scrollTo(0, (int) Math.ceil((cur-FROM_ANGLE_RAD)*factor));
+    }
   }
   
   private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
@@ -74,7 +173,7 @@ public class Main extends FragmentActivity {
     @Override
     public void onVoiceServiceConnected() {
       mPower = new PowerHelper(getApplicationContext());
-      voiceInputHelper.setVoiceConfig(INITIAL_VC, false); // screenoff=true
+      mVoiceInputHelper.setVoiceConfig(INITIAL_VC, false); // screenoff=true
     }
     
     @Override
--- src/de/tud/labAssist/ScreenSlidePageFragment.java
+++ src/de/tud/labAssist/ScreenSlidePageFragment.java
@@ -2,6 +2,7 @@ package de.tud.labAssist;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +11,27 @@ import android.widget.TextView;
 public class ScreenSlidePageFragment extends Fragment {
 
   private String text;
+  private int mNum;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    ViewGroup root = (ViewGroup) inflater.inflate(R.layout.scrollable_card, container, false);
-    TextView tv = (TextView) root.findViewById(R.id.textView1);
+    ViewGroup root = (ViewGroup) inflater.inflate(R.layout.scrollable_typophile, container, false);
+    TextView tv = (TextView) root.findViewById(R.id.typophile_card);
     tv.setText(text);
+    
+    root.setTag(String.format("card_view_%d", mNum)); // so we can find it later
+    tv.setTag(String.format("fu_%d",mNum));
     return root;
   }
 
   public void setNum(int num) {
-    text = String.format("Hello World! %d",num);
+    text = "";
+    mNum = num;
+    
+    for (int i=0; i<num+1; i++) {
+      text += String.format("Hello World! %d",num);
+    } 
   }
 
 }
