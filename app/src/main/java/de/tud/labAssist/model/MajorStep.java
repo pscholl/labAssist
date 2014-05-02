7c506012e2cc71ca9bfe32f18527be816545d6c5
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2014-05-02 10:27:41 +0200

    introducing Data model

---
+++ app/src/main/java/de/tud/labAssist/model/MajorStep.java
@@ -0,0 +1,14 @@
+package de.tud.labAssist.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ramon on 28.04.2014.
+ */
+public class MajorStep {
+
+	private ArrayList<MinorStep> minorSteps;
+	private String description;
+
+}
+
---
+++ app/src/main/java/de/tud/labAssist/model/MinorStep.java
@@ -0,0 +1,8 @@
+package de.tud.labAssist.model;
+
+/**
+ * Created by Ramon on 28.04.2014.
+ */
+public class MinorStep {
+
+}
