02fd80763cf8e707cd0a39b738979d17048618e9
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2014-11-17 21:19:01 +0100

    ~ missing file

---
+++ app/src/main/java/de/tud/labAssist/model/Protocol.java
@@ -0,0 +1,38 @@
+package de.tud.labAssist.model;
+
+import android.content.Context;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+import de.tud.labAssist.model.io.MarkdownManager;
+import de.tud.labAssist.model.steps.MajorStep;
+import de.tud.labAssist.resources.ResourceCache;
+
+/**
+ * Created by Ramon on 04.11.2014.
+ */
+public class Protocol {
+	private final List<MajorStep> steps;
+	private final MarkdownManager.Document origin;
+
+	public Protocol(List<MajorStep> steps, MarkdownManager.Document origin) {
+		this.origin = origin;
+		this.steps = steps;
+	}
+
+	public List<MajorStep> getSteps() {
+		return steps;
+	}
+
+	public void saveState(Context context) {
+		ObjectMapper mapper = ResourceCache.getJsonMapper();
+		try {
+			mapper.writeValue(origin.getStateOutput(context), this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
